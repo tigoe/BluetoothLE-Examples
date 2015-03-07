bf854191cf6c75f7d1bdba380becf625b515ee20
Don Coleman <dcoleman@chariotsolutions.com>
2015-03-06 19:20:57 -0500

    remove

--- bleno/ibeacon/index.js
+++ bleno/ibeacon/index.js
@@ -1,8 +1,6 @@
 var bleno = require('bleno');
 
 var uuid = 'ad5d9ece-9331-48c2-b597-2845aac4a8f0';
-//var uuid = 'ad5d9ece-9331-48c2-b597-2845aac4a8f0'.replace(/-/g, '');
-//var uuid = 'ad5d9ece933148c2b5972845aac4a8f0';
 var major = 0x12; // 0x0000 - 0xffff
 var minor = 0x08; // 0x0000 - 0xffff
 var measuredPower = -18; // -128 - 127
