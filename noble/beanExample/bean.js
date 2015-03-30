517ae6eb5f974be524eba0aad979fcf949ce692e
mariapaulasaba <mariapaula.saba@nyu.edu>
2015-03-29 22:06:01 -0400

    bean scratch examples

---
+++ noble/beanExample/bean.js
@@ -0,0 +1,70 @@
+/*jslint node: true */
+"use strict";
+
+/* 
+ * This script listens for data on the first two (of five available) Scratch characteristics
+ * when this sketch is programmed to the Arduino:
+ * https://punchthrough.com/bean/the-arduino-reference/setscratchdata/
+ */
+
+var Bean = require('ble-bean');
+
+var interval;
+var connectedBean;
+
+Bean.discover(function(bean){
+  connectedBean = bean;
+  console.log("bean discovered");
+  process.on('SIGINT', exitHandler.bind(this));
+
+  // bean.on("serial", function(data, valid){
+  //   console.log(data.toString());
+  // });
+
+  bean.on("disconnect", function(){
+    process.exit();
+  });
+
+  bean.connectAndSetup(function(){
+
+    var writeData = function(){
+
+      bean.writeOne(new Buffer([getRandomInt(0,255),getRandomInt(0,255),getRandomInt(0,255)]),
+      //called when theres data
+      function(){
+                 console.log("data written");
+  
+      });
+    }
+
+     interval = setInterval(writeData,1000);
+
+  console.log("bean connected and setup");
+
+  });
+
+});
+
+process.stdin.resume();//so the program will not close instantly
+var triedToExit = false;
+
+//turns off led before disconnecting
+var exitHandler = function exitHandler() {
+
+  var self = this;
+  if (connectedBean && !triedToExit) {
+    triedToExit = true;
+    console.log('Turning off led...');
+    clearInterval(interval);
+    connectedBean.setColor(new Buffer([0x0,0x0,0x0]), function(){});
+    //no way to know if succesful but often behind other commands going out, so just wait 2 seconds
+    console.log('Disconnecting from Device...');
+    setTimeout(connectedBean.disconnect.bind(connectedBean, function(){}), 2000);
+  } else {
+    process.exit();
+  }
+};
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
