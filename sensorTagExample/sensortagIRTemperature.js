c5d66f614b4a299940deeabd4a23e8a28770af23
tigoe <tom.igoe@gmail.com>
2015-04-07 17:08:15 -0400

    Added sensorTag IR temperature example

---
+++ sensorTagExample/sensortagIRTemperature.js
@@ -0,0 +1,88 @@
+/*
+	sensorTag IR Temperature sensor example
+
+	This example uses Sandeep Mistry's sensortag library for node.js to
+	read data from a TI sensorTag.
+
+	Although the sensortag library functions are all asynchronous,
+	there is a sequence you need to follow in order to successfully
+	read a tag:
+		1) discover the tag
+		2) connect to the tag
+		3) read its services and characteristics
+		4) turn on the sensor you want to use (in this case, accelerometer)
+		5) turn on notifications for the sensor
+		6) listen for changes from the sensortag
+
+	This example does all of those steps in sequence by having each function
+	call the next as a callback. Discover calls connect, which calls
+	discoverServicesAndCharacteristics, and so forth.
+
+	This example is heavily indebted to Sandeep's test for the library, but
+	achieves more or less the same thing without using the async library.
+
+	created 15 Jan 2015
+	modified 7 April 2015
+	by Tom Igoe
+*/
+
+
+var SensorTag = require('sensortag');		// sensortag library
+
+// listen for tags:
+SensorTag.discover(function(tag) {
+	// when you disconnect from a tag, exit the program:
+	tag.on('disconnect', function() {
+		console.log('disconnected!');
+		process.exit(0);
+	});
+
+	function connectMe() {			// attempt to connect to the tag
+     console.log('connect');
+     tag.connect(discoverMe);		// when you connect, call discoverMe
+   }
+
+   function discoverMe() {			// attempt to discover services
+     console.log('discoverServicesAndCharacteristics');
+     // when you discover services, enable the accelerometer:
+     tag.discoverServicesAndCharacteristics(enableIrTempMe);
+   }
+
+   function enableIrTempMe() {		// attempt to enable the IR Temperature sensor
+     console.log('enableIRTemperatureSensor');
+     // when you enable the IR Temperature sensor, start notifications:
+     tag.enableIrTemperature(notifyMe);
+   }
+
+	function notifyMe() {
+   	tag.notifyIrTemperature(listenForTempReading);   	// start the accelerometer listener
+		tag.notifySimpleKey(listenForButton);		// start the button listener
+   }
+
+   // When you get an accelermeter change, print it out:
+	function listenForTempReading() {
+		tag.on('irTemperatureChange', function(objectTemp, ambientTemp) {
+	     console.log('\tObject Temp = %d deg. C', objectTemp.toFixed(1));
+	     console.log('\tAmbient Temp = %d deg. C', ambientTemp.toFixed(1));
+	   });
+	}
+
+	// when you get a button change, print it out:
+	function listenForButton() {
+		tag.on('simpleKeyChange', function(left, right) {
+			if (left) {
+				console.log('left: ' + left);
+			}
+			if (right) {
+				console.log('right: ' + right);
+			}
+			// if both buttons are pressed, disconnect:
+			if (left && right) {
+				tag.disconnect();
+			}
+	   });
+	}
+
+	// Now that you've defined all the functions, start the process:
+	connectMe();
+});
