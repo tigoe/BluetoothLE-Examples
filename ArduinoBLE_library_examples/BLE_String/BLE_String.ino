556eda3c9d356aefbc64e147d36dc09e43055150
Tom Igoe <tom.igoe@gmail.com>
2022-03-07 06:34:59 -0500

    Update BLE_String.ino

--- ArduinoBLE_library_examples/BLE_String/BLE_String.ino
+++ ArduinoBLE_library_examples/BLE_String/BLE_String.ino
@@ -3,10 +3,14 @@
 
   Creates two string characteristics, one for receive and one for transmit.
   When a central connects, it listens for incoming on the rxCharacteristic,
-  and sends incoming Serial out on the txCharacteristic. 
+  and sends incoming Serial out on the txCharacteristic.
+
+  rewritten to use event handlers Mar 2022.
+  
   Thanks to Ami Mehta for the suggestion to write this.
 
   created 16 Mar 2021
+  modified 22 Mar 2022
   by Tom Igoe
 */
 
@@ -16,11 +20,11 @@
 const int characteristicSize = 128;
 
 // create service and characteristics:
-BLEService stringService("7DEF8317-7300-4EE6-8849-46FACE74CA2A"); 
-BLEStringCharacteristic txCharacteristic("7DEF8317-7301-4EE6-8849-46FACE74CA2A", 
-BLERead | BLENotify, characteristicSize);
-BLEStringCharacteristic rxCharacteristic("7DEF8317-7302-4EE6-8849-46FACE74CA2A", 
-BLEWrite, characteristicSize);
+BLEService stringService("7DEF8317-7300-4EE6-8849-46FACE74CA2A");
+BLEStringCharacteristic txCharacteristic("7DEF8317-7301-4EE6-8849-46FACE74CA2A",
+    BLERead | BLENotify, characteristicSize);
+BLEStringCharacteristic rxCharacteristic("7DEF8317-7302-4EE6-8849-46FACE74CA2A",
+    BLEWrite, characteristicSize);
 
 void setup() {
   // initialize serial and BLE:
@@ -29,6 +33,8 @@ void setup() {
     Serial.println("starting BLE failed!");
     while (true);
   }
+  // use the builtin LED as a connect/disconnect handler:
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // set the local name peripheral advertises:
   BLE.setLocalName("BLE_String");
@@ -38,7 +44,14 @@ void setup() {
   // add the characteristics to the service:
   stringService.addCharacteristic(txCharacteristic);
   stringService.addCharacteristic(rxCharacteristic);
-  
+
+  // assign event handlers for connected, disconnected to peripheral
+  BLE.setEventHandler(BLEConnected, connectHandler);
+  BLE.setEventHandler(BLEDisconnected, disconnectHandler);
+
+  // assign event handlers for characteristic
+  rxCharacteristic.setEventHandler(BLEWritten, incomingDataHandler);
+
   // add the service and set a value for the characteristic:
   BLE.addService(stringService);
   // start advertising
@@ -46,27 +59,31 @@ void setup() {
 }
 
 void loop() {
-  // Listen for peripherals:
-  BLEDevice central = BLE.central();
+  // Listen for events:
+  BLE.poll();
 
-  // If a central connects:
-  if (central) {
-      // print its address:
-      Serial.println(central.address());
-    // while it's connected:
-      while (central.connected()) {
-      // if the central writes to the rxCharacteristic, print it:
-      if (rxCharacteristic.written()) {
-        Serial.println(rxCharacteristic.value());
-      }
-      // if a serial string comes in, set it in the  txCharacteristic:
-      if (Serial.available()) {
-        txCharacteristic.setValue(Serial.readString());         
-      }  
-    }
-    // print a disconnect message:
-    Serial.print("Central ");
-    Serial.print(central.address());
-    Serial.println(" disconnected");
+  // if a serial string comes in, set it in the txCharacteristic:
+  if (Serial.available()) {
+    txCharacteristic.setValue(Serial.readString());
   }
+}
+
+void connectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void disconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void incomingDataHandler(BLEDevice central, BLECharacteristic characteristic) {
+  // central wrote new value to characteristic, update LED
+  Serial.print("Characteristic event, written: ");
+  Serial.println(rxCharacteristic.value());
 }
